125e580f44f9a5a1441429fa9b2e08a2307c4381
fabrice CONCHOU <fabrice.conchou@laposte.net>
2019-04-01 12:01:00 +0200

    Commit with responsive Portrait

---
+++ Zozor+/MODEL/countOnMeModel.swift
@@ -0,0 +1,35 @@
+//
+//  countOnMeModel.swift
+//  CountOnMe
+//
+//  Created by utilisateur on 28/03/2019.
+//  Copyright © 2019 Ambroise Collon. All rights reserved.
+//
+
+import Foundation
+
+class Calculate {
+    
+    // MARK: - Properties
+//    var stringNumbers: [String] = [String()]
+ 
+//var operators: [String] = ["+"]
+ //   var index = 0
+  //  var isExpressionCorrect: Bool {
+   //     if let stringNumber = stringNumbers.last {
+     //       if stringNumber.isEmpty {
+       //         if stringNumbers.count == 1 {
+         //           let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
+           //         alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+           //         self.present(alertVC, animated: true, completion: nil)
+    //    } else {
+      //              let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
+       //             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+         //           self.present(alertVC, animated: true, completion: nil)
+           //     }
+           //     return false
+         //   }
+      //  }
+     //   return true
+   // }
+}
